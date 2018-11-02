05186b000e409062d57d5d8598abb312b2605ae5
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-11-01 23:15:10 -0400

    updated production settings

--- emap/production_settings.py
+++ emap/production_settings.py
@@ -81,7 +81,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'emap',
         'USER': 'clayton',
-        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
