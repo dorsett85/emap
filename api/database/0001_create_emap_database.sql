0b6a15cd33d6fe2b974bb66909797fa12bf99dc4
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-11-01 22:48:37 -0400

    Added local database setup script

---
+++ api/database/0001_create_emap_database.sql
@@ -0,0 +1,3 @@
+-- Initial setup for api app
+CREATE USER clayton;
+CREATE DATABASE emap OWNER clayton;
--- api/database/0001_create_game_table.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE games (
-  ID int NOT NULL AUTO_INCREMENT,
-  description VARCHAR(500),
-  PRIMARY KEY (ID)
-);
