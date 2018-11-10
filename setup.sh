b2b2878a24ea8877870572176625851c79664639
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-11-10 00:19:22 -0500

    updated setup.sh to run custom sql scripts

--- setup.sh
+++ setup.sh
@@ -37,5 +37,8 @@ $pyenv
 python manage.py migrate
 python manage.py createsuperuser
 
+# Now that the initial django tables have been created we can run the rest of our sql scripts
+$sudo $psql -f api/database/0002_create_game_table.sql
+
 # Add fixture data
 python manage.py loaddata worldcities100.json
