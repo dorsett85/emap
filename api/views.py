74610e6bf682d62a80e2a0f47653096a43da0fcb
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-11-12 22:19:55 -0500

    server test

--- api/views.py
+++ api/views.py
@@ -63,7 +63,7 @@ def get_place(request, name):
 def get_games(request):
     if not request.is_ajax():
         return HttpResponse('Must be an ajax request')
-
+    return JsonResponse('', safe=False)
     # cursor = connection.cursor()
     # cursor.execute('SELECT name, description, num_questions, difficulty FROM api_game')
     # rows = cursor.fetchall()
