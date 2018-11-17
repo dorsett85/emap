cc49c972d35bf7eae75e3e44d6db7c69959bd72e
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-11-17 14:54:58 -0500

    server test

--- setup.sh
+++ setup.sh
@@ -5,48 +5,48 @@
 # development or production
 #####
 
-# Add appropriate variables for development vs production setup
 if [ -z ${1+x} ] || [ $1 = '-d' ]; then
   echo 'Running development setup'
-  settingEnv='development'
-  sudo=''
-  psql='psql -U postgres'
-  pyenv='source pyenv/Scripts/activate'
+  cp emap/development_settings.py emap/settings.py
+
+  # Create database and user owner
+  psql -U postgres -f api/database/migrations/0001_create_emap_database.sql
+
+  # Activate python virtual environment
+  source pyenv/Scripts/activate
+
+  # Run migrations -- Adds Django specific user and authentication tables -- and create super user
+  manage.py migrate
+  manage.py createsuperuser
+
+  # Now that the django tables have been created we can create add our own tables and relations
+  psql -U postgres -d emap -f api/database/migrations/0002_create_game_table.sql
+  psql -U postgres -d emap -f api/database/migrations/0003_create_city_table.sql
+
+  # load in data
+  psql -U postgres -d emap -c "\copy api_game(name, description, num_questions, difficulty) \
+    FROM 'api/database/data/games.csv' WITH DELIMITER ',' CSV HEADER"
+
+  psql -U postgres -d emap -c "\copy api_city(name, lat, lon, country, population) \
+    FROM 'api/database/data/worldcities100.csv' WITH DELIMITER ',' CSV HEADER"
+
+  # Dump database to setup on production
+  pg_dump -U postgres -Fc emap > api/database/dump/emap.dump
+
 elif [ $1 = '-p' ]; then
   echo 'Running production setup'
-  settingEnv='production'
-  sudo='sudo'
-  psql='-u postgres psql'
-  pyenv='. pyenv/bin/activate'
+  sudo cp emap/production_settings.py emap/settings.py
+
+  # Remove old database and user
+  sudo -u postgres dropdb emap
+  sudo -u postgres dropuser clayton
+
+  # Create database and user owner
+  sudo -u postgres psql -f api/database/migrations/0001_create_emap_database.sql
+
+  # Restore database built from development
+  sudo -u postgres pg_restore -C -d postgres api/database/dump/emap.dump
+  
 else
   echo 'Set argument to -d (development) or -p (production)'
 fi
-
-
-##### Initialize settings and database now that development or production variables are set #####
-
-$sudo cp emap/${settingEnv}_settings.py emap/settings.py
-
-# Create database and user owner
-$sudo $psql -f api/database/migrations/0001_create_emap_database.sql
-
-# Activate python virtual environment
-$pyenv
-
-# Run migrations -- Adds Django specific user and authentication tables -- and create super user
-python manage.py migrate
-python manage.py createsuperuser
-
-# Now that the django tables have been created we can create add our own tables and relations
-$sudo $psql -d emap -f api/database/migrations/0002_create_game_table.sql
-$sudo $psql -d emap -f api/database/migrations/0003_create_city_table.sql
-
-# load in data
-$sudo $psql -d emap -c "\copy api_game(name, description, num_questions, difficulty) \
-  FROM 'api/database/data/games.csv' WITH DELIMITER ',' CSV HEADER"
-
-$sudo $psql -d emap -c "\copy api_city(name, lat, lon, country, population) \
-  FROM 'api/database/data/worldcities100.csv' WITH DELIMITER ',' CSV HEADER"
-
-# Dump database to setup on production
-pg_dump -U postgres -Fc emap > api/database/dump/emap.dump
